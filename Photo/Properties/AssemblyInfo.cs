76e8fd6fda3d8daf173d50ebab3c9bb25687953d
Dylan K. Bickerstaff <dylan@belowaverage.org>
2020-02-19 13:19:32 -0500

    Add project files.

---
+++ Photo/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("below average - AD Photo API")]
+[assembly: AssemblyDescription("below average - AD Photo API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("below average")]
+[assembly: AssemblyProduct("below average - AD Photo API")]
+[assembly: AssemblyCopyright("Copyright © 2020 - Dylan Bickerstaff")]
+[assembly: AssemblyTrademark("below average")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("de094fa9-63e1-4789-9af5-e670020533c5")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
---
+++ Photo/default.ashx.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.DirectoryServices;
+using System.Text.RegularExpressions;
+
+namespace Photo
+{
+    public class _default : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+        public void ProcessRequest(HttpContext http)
+        {
+            string url = http.Request.RawUrl;
+            if (url.Length >= 3 && url.StartsWith("/?"))
+            {
+                string userName = url.Replace("/?", "");
+                if (userName != null && Regex.IsMatch(userName, @"[\w\d]*[^/\\[\]:;|=,+*?<>]*"))
+                {
+                    DirectorySearcher ds = new DirectorySearcher("(&(objectClass=user)(sAMAccountName=" + userName + "))", new string[] { "thumbnailPhoto" });
+                    SearchResult result = ds.FindOne();
+                    if (result != null)
+                    {
+                        http.Response.ContentType = "image/jpeg";
+                        http.Response.BinaryWrite((byte[])result.Properties["thumbnailPhoto"][0]);
+                        return;
+                    }
+                }
+            }
+            http.Response.ContentType = "text/plain";
+            http.Response.Write("Please specify a valid User Name, E.G: ./?username");
+        }
+    }
+}
