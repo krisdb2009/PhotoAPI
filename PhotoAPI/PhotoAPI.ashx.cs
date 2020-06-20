b641c2d4f4e99619ccb30cdbc66dccafb1b54b78
Dylan Bickerstaff <dylan@belowaverage.org>
2020-06-19 23:52:07 -0400

    Fixed Filter Bug

--- PhotoAPI/PhotoAPI.ashx.cs
+++ PhotoAPI/PhotoAPI.ashx.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web;
 using System.DirectoryServices;
 using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace PhotoAPI
 {
@@ -15,20 +17,29 @@ namespace PhotoAPI
             if (http.Request.QueryString.Count == 1)
             {
                 string userName = http.Request.QueryString[0];
-                if (userName != null && Regex.IsMatch(userName, @"[\w\d]*[^/\\[\]:;|=,+*?<>]*"))
+                foreach(KeyValuePair<string, string> esc in replace)
                 {
-                    DirectorySearcher ds = new DirectorySearcher("(&(objectClass=user)(sAMAccountName=" + userName + "))", new string[] { "thumbnailPhoto" });
-                    SearchResult result = ds.FindOne();
-                    if (result != null && result.Properties.Contains("thumbnailphoto"))
-                    {
-                        http.Response.ContentType = "image/jpeg";
-                        http.Response.BinaryWrite((byte[])result.Properties["thumbnailphoto"][0]);
-                        return;
-                    }
+                    userName = userName.Replace(esc.Key, esc.Value);
+                }
+                DirectorySearcher ds = new DirectorySearcher("(&(objectClass=user)(sAMAccountName=" + userName + "))", new string[] { "thumbnailPhoto" });
+                SearchResult result = ds.FindOne();
+                if (result != null && result.Properties.Contains("thumbnailphoto"))
+                {
+                    http.Response.ContentType = "image/jpeg";
+                    http.Response.BinaryWrite((byte[])result.Properties["thumbnailphoto"][0]);
+                    return;
                 }
             }
             http.Response.ContentType = "text/plain";
             http.Response.Write("Please specify a valid User Name, E.G: ./?username");
         }
+        private Dictionary<string, string> replace = new Dictionary<string, string>() {
+            { "*", @"\2a" },
+            { "(", @"\28" },
+            { ")", @"\29" },
+            { @"\", @"\5c" },
+            { "NUL", @"\00" },
+            { "/", @"\2f" }
+        };
     }
 }
